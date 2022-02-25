cd39cac3599411aa166d72d88e35d4b943a70222
Filip <filip.unger@hotmail.se>
2022-02-25 10:25:51 +0100

    Back up!

--- src/main/java/Operations.java
+++ src/main/java/Operations.java
@@ -6,9 +6,6 @@ import javax.swing.*;
 public class Operations implements ActionListener {
 
     static int summary = 0;
-    static int currentLeft = 0;
-    static int currentRight = 0;
-    static boolean onLeft = true;
 
     // Function that catches the click of a button
     public void actionPerformed(ActionEvent event) {
@@ -16,15 +13,7 @@ public class Operations implements ActionListener {
         System.out.println(command);
 
         // If a number
-        if (isNumeric(command)) {
-            if(onLeft)
-            {
-                currentLeft = (currentLeft * 10) + Integer.parseInt(command);
-            }
-            else
-            {
-                currentRight = (currentRight * 10) + Integer.parseInt(command);
-            }
+        if () {
         }
         // Addition
         else if (command == "+") {
@@ -53,43 +42,5 @@ public class Operations implements ActionListener {
         else {
             System.out.println("Unhandled input: " + command);
         }
-    }
-
-    // Helper function to figure out of if the string is a numerical
-    // https://www.baeldung.com/java-check-string-number
-    public static boolean isNumeric(String strNum) {
-        if (strNum == null) {
-            return false;
-        }
-        try {
-            Integer.parseInt(strNum);
-        } catch (NumberFormatException nfe) {
-            return false;
-        }
-        return true;
-    }
-
-    // Functions for calculations
-    public int addition(int left, int right) {
-        return left + right;
-    }
-
-    public int subraction(int left, int right) {
-        return left - right;
-    }
-
-    public int division(int left, int right) {
-        return left / right;
-    }
-
-    public int multiplication(int left, int right) {
-        return left * right;
-    }
-
-    public int equals() {
-        return -1;
-    }
-
-    public void deletion() {
     }
 }
