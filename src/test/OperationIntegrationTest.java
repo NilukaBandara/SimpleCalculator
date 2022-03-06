1286e4faadecb0f577ec4d4ae254876ecd0388df
Filip <filip.unger@hotmail.se>
2022-03-06 15:08:27 +0100

    Attempting to make an integration test

--- src/test/OperationIntegrationTest.java
+++ src/test/OperationIntegrationTest.java
@@ -1,25 +1,31 @@
 import org.junit.Test;
 
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import static org.junit.Assert.*;
-public class OperationIntegrationTest {
+public class OperationIntegrationTest implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //Nothing
+    }
 
     @Test
     public void testSomeFakeInputs()
     {
         //Setup
         Operations op = new Operations();
-        UserInterface ui;
-        ActionEvent fakeEvent = new ActionEvent(ui, 1001, "1");
+        OperationIntegrationTest fakeObj = new OperationIntegrationTest();
+        ActionEvent fakeEvent = new ActionEvent(fakeObj, 1001, "1");
 
         //Test
         op.actionPerformed(fakeEvent);
 
-        fakeEvent = new ActionEvent(ui, 1001, "+");
+        fakeEvent = new ActionEvent(fakeObj, 1001, "+");
         op.actionPerformed(fakeEvent);
 
-        fakeEvent = new ActionEvent(ui, 1001, "1");
+        fakeEvent = new ActionEvent(fakeObj, 1001, "1");
         op.actionPerformed(fakeEvent);
 
         double two = 2;
