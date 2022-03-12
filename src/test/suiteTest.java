8ff2b3bdea87e28955218803a3e690bdd76db0d0
kashifmahmood86 <kashifmahmood86@gmail.com>
2022-03-13 00:57:21 +0100

    write suittest class

---
+++ src/test/suiteTest.java
@@ -0,0 +1,16 @@
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ additionTest.class, OperationIntegrationTest.class })
+
+public class suiteTest {
+
+}
