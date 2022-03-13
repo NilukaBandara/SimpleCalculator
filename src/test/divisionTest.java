41f360a51715e1f5a8198440e619dd7e993b0b76
Sabahat Saeed <sabahatsaeed31@gmail.com>
2022-03-13 18:53:37 +0100

    Create divisionTest.java

---
+++ src/test/divisionTest.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class divisionTest {
+    public Operations operations = new Operations();
+    @Test
+    public void testDivisionFail() {
+        //Setup
+        Operations operations = new Operations();
+        double expected = 3.0;
+        //Test
+        double actual = operations.performDivision("10","5");
+        assertNotEquals(expected, actual, 0.0);
+
+    }
+    @Test
+    public void testDivisionPass() {
+        //Setup
+        Operations operations = new Operations();
+        double expected = 5.0;
+        //Test
+        double actual = operations.performDivision("10", Double.toString(operations.performDivision("10","5")));
+        assertEquals(expected, actual, 0.0);
+
+    }
+}
