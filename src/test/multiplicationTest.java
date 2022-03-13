cbe303e39becdd2a50db464a6fb91a9a1a989fb8
Sabahat Saeed <sabahatsaeed31@gmail.com>
2022-03-13 18:51:14 +0100

    Create multiplicationTest.java

---
+++ src/test/multiplicationTest.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class multiplicationTest {
+    public Operations operations = new Operations();
+    @Test
+    public void testMultiplicationFail() {
+        //Setup
+        Operations operations = new Operations();
+        double expected = 37.0;
+        //Test
+        double actual = operations.performMultiplication("6","6");
+        assertNotEquals(expected, actual, 0.0);
+
+    }
+    @Test
+    public void testMultiplicationPass() {
+        //Setup
+        Operations operations = new Operations();
+        double expected = 36.0;
+        //Test
+        double actual = operations.performMultiplication("6", Double.toString(operations.performMultiplication("3","2")));
+        assertEquals(expected, actual, 0.0);
+
+    }
+}
