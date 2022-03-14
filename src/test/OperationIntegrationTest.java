1ee9bd9ccd62886ff97fc92a633e0d5e9953f894
NilukaBandara <nilukapribandara@gmail.com>
2022-03-14 08:13:30 +0100

    Modified suiteTest and IntegrationTest

--- src/test/OperationIntegrationTest.java
+++ src/test/OperationIntegrationTest.java
@@ -1,8 +1,6 @@
 import org.junit.Test;
 
-import javax.swing.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import static org.junit.Assert.*;
 public class OperationIntegrationTest {
@@ -12,68 +10,7 @@ public class OperationIntegrationTest {
     }
 
     @Test
-    public void OperationsIntegrationTest()
-    {
-        //Setup
-        Operations op = new Operations();
-        UserInterface ui = new UserInterface();
-        ui.m_texttWindow = new JTextField();
-        ActionEvent event;
-        double answer = 0;
-
-        //Test
-        event = new ActionEvent(ui, 1001, "1");
-        op.actionPerformed(event);
-
-        event = new ActionEvent(ui, 1001, "1");
-        op.actionPerformed(event);
-
-        event = new ActionEvent(ui, 1001, "+");
-        op.actionPerformed(event);
-
-        event = new ActionEvent(ui, 1001, "9");
-        op.actionPerformed(event);
-
-        //11 + 9  = 20
-        answer = 11 + 9;
-
-        event = new ActionEvent(ui, 1001, "*");
-        op.actionPerformed(event);
-
-        event = new ActionEvent(ui, 1001, "4");
-        op.actionPerformed(event);
-
-        //20 * 4 = 80
-        answer = answer * 4;
-
-        event = new ActionEvent(ui, 1001, "/");
-        op.actionPerformed(event);
-
-        event = new ActionEvent(ui, 1001, "8");
-        op.actionPerformed(event);
-
-        // 80 / 8 = 10
-        answer = answer / 8;
-
-        event = new ActionEvent(ui, 1001, "-");
-        op.actionPerformed(event);
-
-        event = new ActionEvent(ui, 1001, "3");
-        op.actionPerformed(event);
-
-        //10 - 3 = 7
-        answer = answer - 3;
-
-        event = new ActionEvent(ui, 1001, "=");
-        op.actionPerformed(event);
-
-        assertEquals(answer, op.finalAnswer, 0.0);
-
-        //Teardown
-        //...
-    }
-    @Test
-    public void performintegrationTest()
+    public void performIntegrationFailTest()
     {
         //Setup
         Operations operations = new Operations();
@@ -85,7 +22,7 @@ public class OperationIntegrationTest {
     }
 
     @Test
-    public void performintegrationPassTest()
+    public void performIntegrationPassTest()
     {
         //Setup
         Operations operations = new Operations();
--- src/test/suiteTest.java
+++ src/test/suiteTest.java
@@ -1,9 +1,10 @@
-import junit.framework.TestCase;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
@@ -12,5 +13,66 @@ import org.junit.runners.Suite.SuiteClasses;
 @SuiteClasses({additionTest.class, OperationIntegrationTest.class, divisionTest.class, multiplicationTest.class, subtractionTest.class, additionTest.class, OperationIntegrationTest.class, divisionTest.class, multiplicationTest.class, subtractionTest.class})
 
 public class suiteTest {
+    @Test
+    public void OperationsTestSuite()
+    {
+        //Setup
+        Operations op = new Operations();
+        UserInterface ui = new UserInterface();
+        ui.m_texttWindow = new JTextField();
+        ActionEvent event;
+        double answer = 0;
+
+        //Test
+        event = new ActionEvent(ui, 1001, "1");
+        op.actionPerformed(event);
+
+        event = new ActionEvent(ui, 1001, "1");
+        op.actionPerformed(event);
+
+        event = new ActionEvent(ui, 1001, "+");
+        op.actionPerformed(event);
+
+        event = new ActionEvent(ui, 1001, "9");
+        op.actionPerformed(event);
+
+        //11 + 9  = 20
+        answer = 11 + 9;
+
+        event = new ActionEvent(ui, 1001, "*");
+        op.actionPerformed(event);
+
+        event = new ActionEvent(ui, 1001, "4");
+        op.actionPerformed(event);
+
+        //20 * 4 = 80
+        answer = answer * 4;
+
+        event = new ActionEvent(ui, 1001, "/");
+        op.actionPerformed(event);
+
+        event = new ActionEvent(ui, 1001, "8");
+        op.actionPerformed(event);
+
+        // 80 / 8 = 10
+        answer = answer / 8;
+
+        event = new ActionEvent(ui, 1001, "-");
+        op.actionPerformed(event);
+
+        event = new ActionEvent(ui, 1001, "3");
+        op.actionPerformed(event);
+
+        //10 - 3 = 7
+        answer = answer - 3;
+
+        event = new ActionEvent(ui, 1001, "=");
+        op.actionPerformed(event);
+
+        assertEquals(answer, op.finalAnswer, 0.0);
+
+        //Teardown
+        //...
+    }
 
 }
